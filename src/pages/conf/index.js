be4fe4dec49e3f24429ac4df974c07aac7e43ebc
KnoveZ <308346293@qq.com>
2018-08-01 14:15:51 +0800

    会议室暂存

--- src/pages/conf/index.js
+++ src/pages/conf/index.js
@@ -1,22 +1,75 @@
 import React from "react";
 import { connect } from "dva";
+import moment from "moment";
 import { Menu, Icon, Table } from "antd";
 import router from "umi/router";
 
+moment.lang("cn");
+
 class Index extends React.Component {
   render() {
-    const h =
-      (window.innerHeight ||
-        document.documentElement.clientHeight ||
-        document.body.clientHeight) - 66;
+    const back_data = [
+      {
+        date_start: 4,
+        date_end: 7,
+        date_day: Date(),
+        type: "1",
+        conf: "2",
+        title: "济南19届新同学入职济南19届新同学入职济南19届新同学入职济南19届新同学入职",
+        num: 44,
+        user: "孙佳勇",
+        user_id: "123123213",
+        id: "!23124asdjasdhjaksdasd"
+      },
+      {
+        date_start: 8,
+        date_end: 12,
+        date_day: Date(),
+        type: "3",
+        conf: "1",
+        title: "计划会议A",
+        num: 11,
+        user: "王勇",
+        user_id: "123123213",
+        id: "A53124sdsdhjaksdasd"
+      },
+      {
+        date_start: 1,
+        date_end: 12,
+        date_day: Date(),
+        type: "4",
+        conf: "5",
+        title: "新零售对餐饮行业的变革",
+        num: 153,
+        user: "王翠燕",
+        user_id: "123123213",
+        id: "A53124sdsdhjaksdasd"
+      }
+    ];
     const columns = [
       {
-        title: "日期",
+        title: (
+          <div
+            style={{
+              textAlign: "center"
+            }}
+          >
+            日期
+          </div>
+        ),
         dataIndex: "date",
-        width: "10%",
+        width: "5%",
         render: (value, row, index) => {
           const obj = {
-            children: value,
+            children: (
+              <div
+                style={{
+                  textAlign: "center"
+                }}
+              >
+                {value}
+              </div>
+            ),
             props: {}
           };
           if (
@@ -45,27 +98,105 @@ class Index extends React.Component {
         }
       },
       {
-        title: "会议室",
-        dataIndex: "conf"
+        title: (
+          <div
+            style={{
+              textAlign: "center"
+            }}
+          >
+            会议室
+          </div>
+        ),
+        dataIndex: "conf",
+        width: "5%",
+        render: (value, row, index) => (
+          <div
+            style={{
+              textAlign: "center"
+            }}
+          >
+            {value}
+          </div>
+        )
       }
     ];
-    for (let i = 1; i <= 48; i++) {
+    for (let i = 0; i <= 24; i++) {
       columns.push({
-        title: "会议室",
+        title: moment("207707278", "YYYYMMDDH")
+          .add(i / 2, "hour")
+          .format("HH:mm"),
         dataIndex: "time" + i,
-
+        width: "3%",
+        render: (value, row, index) => {
+          if (value && value.type && value.type.indexOf("k") < 0) {
+            return {
+              children: <div className={"full conf-lv" + value.type} ><span className="td-text">{value.user}（{value.num}人）{value.title}</span></div>,
+              props: {
+                colSpan: value.date_end - value.date_start + 1
+              }
+            };
+          } else if (value) {
+            return {
+              children: <div className={"full conf-lv" + value.type} />,
+              props: {
+                colSpan: 0
+              }
+            };
+          }
+          return {
+            children: <div className={"full conf-lv" + value} />,
+            props: {
+              colSpan: 1
+            }
+          };
+        }
       });
     }
-
+    const confArray = [
+      "采桑子",
+      "如梦令",
+      "画唐春",
+      "西江月",
+      "定风波",
+      "过龙门"
+    ];
     const data = [];
     for (let i = 1; i <= 42; i++) {
-      data.push({
+      // 初始化数据
+      const tableDay = moment(moment(Date()).format("YYYYMMDD"), "YYYYMMDD")
+        .add(4 * (i - 1), "hour")
+        .format("YYYY/MM/DD");
+      console.log(tableDay);
+      const daydata = {
         key: i,
-        date: "2018年5月8日 星期五"
+        date: (
+          <>
+            {tableDay} <br />
+            {moment()
+              .add(i / 6, "DAY")
+              .format("dddd")}
+          </>
+        ),
+        conf: confArray[(i - 1) % 6]
+      };
+      // 获取日期数据
+      back_data.map(item => {
+        console.log((i - 1) % 6, parseInt(item.conf));
+        if (
+          moment(item.date_day).format("YYYY/MM/DD") === tableDay &&
+          parseInt(item.conf) === (i - 1) % 6
+        ) {
+          console.log((i - 1) % 6, parseInt(item.conf), "done");
+          daydata["time" + item.date_start] = item;
+          for (let i = 1; i <= item.date_end - item.date_start; i++)
+            daydata["time" + (item.date_start + i )] = item.type + "k";
+        }
       });
+
+      data.push(daydata);
     }
     return (
-      <div style={{ minHeight: h }} className="main-div">
+      <div className="main-div conf">
         <Table
           columns={columns}
           dataSource={data}
